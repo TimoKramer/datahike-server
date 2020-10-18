99d982cc988032da71c7808443ed0cc57e172823
Timo Kramer <fw-git@timokramer.de>
2020-10-18 11:45:48 +0200

    update datahike

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
-                 [io.replikativ/datahike "0.3.2"]
+                 [io.replikativ/datahike "0.3.3-SNAPSHOT" :exclusions [io.replikativ/konserve io.replikativ/superv.async]]
+                 [io.replikativ/datahike-jdbc "0.1.1-SNAPSHOT"]
                  [buddy/buddy-auth "2.2.0"]
                  [ring/ring-core "1.8.2"]
                  [ring/ring-jetty-adapter "1.8.2"]
